93ef719b5f294c89ec497c3383f3ae99de879671
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-09-22 10:10:21 +0200

    CC1 submited

--- 03_CC1/main.py
+++ 03_CC1/main.py
@@ -5,26 +5,54 @@ import arcade
 from agent import Agent
 from conf import *
 from environment import Environment
-from plt_exporter import PLTExporter
+from plt_exporter import extract_history
 from window import MazeWindow
 
-# À expérimenter
-#
-# - Traverser les murs
-# - Plusieurs solutions
 
-if __name__ == "__main__":
-    environment = Environment(MAZE, brickwalls=True)
-    agent = Agent(environment, alpha=1, gamma=0.3, cooling_rate=0.99)
+def run(
+        brickwalls: bool = False,
+        alpha: float = 0.8,
+        gamma: float = 0.3,
+        exploration: float = 1,
+        min_exploration: float = 0.02,
+        cooling_rate: float = 0.99,
+        nb_learn: int = 20,
+        save_state: bool = True,
+        display_game: bool = True,
+        show_graph: bool = True
+):
+    environment = Environment(MAZE, brickwalls=brickwalls)
+    agent = Agent(
+        environment,
+        alpha=alpha,
+        gamma=gamma,
+        exploration=exploration,
+        min_exploration=min_exploration,
+        cooling_rate=cooling_rate
+    )
 
-    if os.path.exists(FILE_QTABLE):
+    if save_state and os.path.exists(FILE_QTABLE):
         agent.load(FILE_QTABLE)
 
-    agent.learn(30)
+    if nb_learn > 0:
+        agent.learn(nb_learn)
 
-    windows = MazeWindow(agent, True)
-    windows.setup()
-    arcade.run()
+    if display_game:
+        windows = MazeWindow(agent, True)
+        windows.setup()
+        arcade.run()
 
-    agent.save(FILE_QTABLE)
-    PLTExporter.extract_history(agent.history)
+    if save_state:
+        agent.save(FILE_QTABLE)
+
+    if show_graph:
+        extract_history(agent.history)
+
+
+if __name__ == "__main__":
+    run(
+        nb_learn=100,
+        brickwalls=False,
+        display_game=False,
+        save_state=False
+    )
--- 03_CC1/plt_exporter.py
+++ 03_CC1/plt_exporter.py
@@ -1,14 +1,9 @@
 import matplotlib
-
-matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
+matplotlib.use('TkAgg')
 
-class PLTExporter:
-    def __init__(self):
-        pass
 
-    @staticmethod
-    def extract_history(history):
-        plt.plot(history)
-        plt.show()
+def extract_history(history):
+    plt.plot(history)
+    plt.show()
